22807ccd85a95afd87b0da71cdee8e0e9c604477
Matthew Baker <mattbaker2281@gmail.com>
2020-01-20 15:32:55 -0600

    worked with DOM to have journal entries be displayed on the web page

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,27 +1,49 @@
 const journalEntry1 = {
-    journalDate: "January 17, 2020",
-    conceptsCovered: "functions and objects in JavaScript",
+    journalDate: "2020-01-10",
+    conceptsCovered: "Functions and objects in JavaScript",
     journalEntry: "I don't know what going on.",
     mood: "Sleepy"
-};
+}
 const journalEntry2 = {
-    journalDate: "August 10, 2020",
-    conceptsCovered: "simple javascript functions",
+    journalDate: "2020-01-10",
+    conceptsCovered: "Simple javascript functions",
     journalEntry: "Still barely know whats going on",
     mood: "Happy"
-};
+}
 const journalEntry3 = {
-    journalDate: "December 24, 2020",
+    journalDate: "2020-01-10",
     conceptsCovered: "Hopefully nothing, ill be out of NSS",
     journalEntry: "Tomorrow is christmas-",
     mood: "Sleepy"
-};
+}
 
 let journalArray = [];
 
 function newJournalEntry (journalEntry) {
-    journalArray.push(journalEntry1, journalEntry2, journalEntry3);
+    journalArray.push(journalEntry);
 }
 
-newJournalEntry(journalEntry);
-console.log(journalArray);
+newJournalEntry(journalEntry1);
+newJournalEntry(journalEntry2);
+newJournalEntry(journalEntry3);
+
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+        <div class="journal">
+            <h1>${journalEntry.conceptsCovered}</h1>
+            <h3>${journalEntry.journalEntry}</h3>
+            <h2>${journalEntry.journalDate}</h2>
+            <h3>${journalEntry.mood}</h3>
+        </div>
+    `
+}
+
+const entryLog = document.getElementById("entryLog");
+
+const renderJournalEntries = (journalArray) => {
+    for (let i = 0; i < journalArray.length; i++) {
+        entryLog.innerHTML += makeJournalEntryComponent(journalArray[i]);
+    }
+}
+
+renderJournalEntries(journalArray);
