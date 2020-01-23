16eaaa581451675e953277823a9760535c8d4ec9
Matthew Baker <mattbaker2281@gmail.com>
2020-01-23 14:30:05 -0600

    used api to pull and display data to the DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,49 +1,30 @@
-const journalEntry1 = {
-    journalDate: "2020-01-10",
-    conceptsCovered: "Functions and objects in JavaScript",
-    journalEntry: "I don't know what going on.",
-    mood: "Sleepy"
-}
-const journalEntry2 = {
-    journalDate: "2020-01-10",
-    conceptsCovered: "Simple javascript functions",
-    journalEntry: "Still barely know whats going on",
-    mood: "Happy"
-}
-const journalEntry3 = {
-    journalDate: "2020-01-10",
-    conceptsCovered: "Hopefully nothing, ill be out of NSS",
-    journalEntry: "Tomorrow is christmas-",
-    mood: "Sleepy"
+const getJournalEntries = () => {
+    const journalUrl = "http://localhost:3000/journalEntries"
+    fetch(journalUrl)
+        .then(resp => resp.json())
+        .then(journalsFromAPI => {
+            renderJournalEntries(journalsFromAPI)
+            console.log(journalsFromAPI)
+        })
 }
 
-let journalArray = [];
-
-function newJournalEntry (journalEntry) {
-    journalArray.push(journalEntry);
+const renderJournalEntries = (journals) => {
+    const container = document.querySelector("#journal-container")
+    journals.forEach(journal => {
+        const journalHTML = journalFactory(journal)
+        container.innerHTML += journalHTML
+    })
 }
 
-newJournalEntry(journalEntry1);
-newJournalEntry(journalEntry2);
-newJournalEntry(journalEntry3);
-
-const makeJournalEntryComponent = (journalEntry) => {
+const journalFactory = (journal) => {
     return `
-        <div class="journal">
-            <h1>${journalEntry.conceptsCovered}</h1>
-            <h3>${journalEntry.journalEntry}</h3>
-            <h2>${journalEntry.journalDate}</h2>
-            <h3>${journalEntry.mood}</h3>
-        </div>
+        <section class="journal">
+            <h1>Date: ${journal.journalDate}</h1>
+            <h1>Concepts Covered: ${journal.conceptsCovered}</h1>
+            <h1>Entry: ${journal.journalEntry}</h1>
+            <h1>Mood: ${journal.mood}</h1>
+        </section>
     `
 }
 
-const entryLog = document.getElementById("entryLog");
-
-const renderJournalEntries = (journalArray) => {
-    for (let i = 0; i < journalArray.length; i++) {
-        entryLog.innerHTML += makeJournalEntryComponent(journalArray[i]);
-    }
-}
-
-renderJournalEntries(journalArray);
+getJournalEntries();
